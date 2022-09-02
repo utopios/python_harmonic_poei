97b797532bd0b947c2b07529c444eff823e35a7a
ihab <ihab@utopios.net>
2022-09-02 15:45:26 +0200

    correction test unique_id

--- corrrection_tasks_tests/test_ex1.py
+++ corrrection_tasks_tests/test_ex1.py
@@ -14,14 +14,33 @@ def test_add_task_returns_valid_id():
 
 def test_added_task_has_id_set():
     """Be sure that the task_id field is set in database by taks.add()"""
-    #Arange
+    #Arrange
     initialized_tasks_db()
     task = Task("do something")
     #Act
     task_id = tasks.add(task)
     task_from_db = tasks.get(task_id)
+    stop_tasks_db()
     #assert
     assert task_from_db.id == task_id
+
+def test_unique_id_1():
+    """Calling unique_id() twice should return different numbers"""
+    initialized_tasks_db()
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
+
+def test_unique_id_2():
+    """unique_id() should return an unused id"""
+    ids = []
+    initialized_tasks_db()
+    ids.append(tasks.add(Task("first")))
+    ids.append(tasks.add(Task("second")))
+
+    uid = tasks.unique_id()
+    stop_tasks_db()
+    assert uid not in ids
 
 def initialized_tasks_db():
     tasks.start_tasks_db(str('temp'), 'tiny')
