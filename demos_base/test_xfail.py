9997a96dbc0ad57e2a5a8a50ab5c3e18a068edea
ihab <ihab@utopios.net>
2022-09-05 10:24:05 +0200

    xfail

--- demos_base/test_xfail.py
+++ demos_base/test_xfail.py
@@ -4,12 +4,12 @@ import pytest
 def inc(x):
     return x + 1
 
-condition = False
+condition = 11
 
 @pytest.mark.xfail
 def test_answer_fail():
     assert inc(3) == 6
 
-@pytest.mark.xfail(condition, strict=True)
+@pytest.mark.xfail(condition > 10, strict=True, reason="xfail when condition gt 10")
 def test_answer_success():
     assert inc(3) == 4
