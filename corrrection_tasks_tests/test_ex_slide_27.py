946429c8b386b8309c26811956850042edbaf7ad
ihab <ihab@utopios.net>
2022-09-05 16:10:37 +0200

    test class expcetion

--- corrrection_tasks_tests/test_ex_slide_27.py
+++ corrrection_tasks_tests/test_ex_slide_27.py
@@ -1,8 +1,20 @@
 from init import *
 import pytest
 
-
+# # uniquement pour l'exemple aucune action sur le test
+# class Class1:
+#     pass
+# class Class2:
+#     pass
 class TestExcpetions():
+
+    # uniquement pour l'exemple aucune action sur le test
+    # @pytest.mark.parametrize("Classe", [Class1, Class2])
+    # def test_add_not_task_raises(self, Classe):
+    #     """add() should raise an exception if wrong type instance"""
+    #     with pytest.raises(TypeError):
+    #         obj = Classe()
+
 
     @pytest.mark.parametrize("method",[tasks.add, tasks.update])
     def test_add_not_task_raises(self, method):
