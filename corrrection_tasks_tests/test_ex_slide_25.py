82bf8fbf2b747577cb2ca6db9fe0fe52131d2ef2
ihab <ihab@utopios.net>
2022-09-05 12:36:23 +0200

    correction ex slide 25

---
+++ corrrection_tasks_tests/test_ex_slide_25.py
@@ -0,0 +1,24 @@
+import pytest
+
+from init import *
+
+@pytest.mark.smoke
+class TestUpdate():
+
+    def equivalent_class(self, t1, t2, task_id):
+        return ((t1.summary == t2.summary) and (t1.owner == t2.owner) and (t1.done == t2.done) and t2.id == task_id)
+
+    def test_update_correct_task(self):
+        """tasks. update(<valid_id>, new task value) should update task"""
+        # Arrange
+        initialized_tasks_db()
+        new_task = Task("new task", done=False)
+        task_id = tasks.add(new_task)
+
+        # Act
+        new_value_task = Task("new value task", done=True)
+        tasks.update(task_id, new_value_task)
+        compare_task = tasks.get(task_id)
+        stop_tasks_db()
+
+        assert self.equivalent_class(new_value_task, compare_task, task_id)
