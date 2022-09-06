79e35e24f28de0508f3e3c56c4c43675ac1a4eb1
ihab <ihab@utopios.net>
2022-09-06 16:43:42 +0200

    exemple hook

--- demo_test_conftest/conftest.py
+++ demo_test_conftest/conftest.py
@@ -5,6 +5,12 @@ import pytest
 def input_value():
     return 33
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_collection(session):
+    ###Execute instructions before builtin pytest_collection
+    yield
+    ###Execute instructions after builtin pytest_collection
+
 def pytest_itemcollected(item):
     pass
 
