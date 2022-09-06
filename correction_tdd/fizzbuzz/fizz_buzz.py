1f8643cf6bae5ec92bbb396af7a3f818754faeb4
ihab <ihab@utopios.net>
2022-09-06 09:19:58 +0200

    test of fizzbuzz

--- correction_tdd/fizzbuzz/fizz_buzz.py
+++ correction_tdd/fizzbuzz/fizz_buzz.py
@@ -1,2 +1,11 @@
 def fizz_buzz(value):
-    pass
+    if not isinstance(value, int):
+        raise TypeError()
+    if value%3 == 0 and value % 5 == 0:
+        return "FizzBuzz"
+    elif value % 3 == 0:
+        return "Fizz"
+    elif value % 5 == 0:
+        return "Buzz"
+    else:
+        return str(value)
