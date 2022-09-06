2c252fd1624dd1cca757c7b4e56ac052c434eff3
ihab <ihab@utopios.net>
2022-09-06 12:18:07 +0200

    correction ex slide 38

---
+++ corrrection_tasks_tests/test_fixture_ex_slide38.py
@@ -0,0 +1,21 @@
+import pytest
+
+from init import *
+
+@pytest.fixture(name="few_tasks")
+def just_a_few_tasks():
+    """create fex tasks"""
+    return (
+        Task("Sample Task"),
+        Task("Second one"),
+        Task("Last one")
+    )
+
+def test_add_with_fixture(few_tasks):
+    initialized_tasks_db()
+    old_count = tasks.count()
+    for t in few_tasks:
+        tasks.add(t)
+    new_count = tasks.count()
+    stop_tasks_db()
+    assert new_count == old_count + len(few_tasks)
