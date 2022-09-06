b4fa127092f6e09ee15f3bd461021b481d9d7897
ihab <ihab@utopios.net>
2022-09-06 16:31:54 +0200

    hooks

--- demo_test_conftest/conftest.py
+++ demo_test_conftest/conftest.py
@@ -4,3 +4,10 @@ import pytest
 @pytest.fixture
 def input_value():
     return 33
+
+def pytest_itemcollected(item):
+    pass
+
+def pytest_runtest_call(item):
+    print(item.name)
+    pass
--- demo_test_conftest/test_conftest.py
+++ demo_test_conftest/test_conftest.py
@@ -1,3 +1,6 @@
 
 def test_with_confttest(input_value):
     assert input_value % 3 == 0
+
+def test_with_confttest_2(input_value):
+    assert input_value % 3 == 0
