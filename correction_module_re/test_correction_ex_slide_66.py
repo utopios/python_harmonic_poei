5c526979b877bb30440bbfbfa42d534a56d3e17f
ihab <ihab@utopios.net>
2022-09-07 16:58:52 +0200

    ex slide 66

---
+++ correction_module_re/test_correction_ex_slide_66.py
@@ -0,0 +1,19 @@
+import re
+
+content = '''start line
+not
+with start
+hello from line'''
+
+def get_line_with_out_start(text):
+    pattern = re.compile(r'start', flags=re.IGNORECASE)
+    lines = re.split('\n', text)
+    result = []
+    for line in lines:
+        if not pattern.search(line):
+            result.append(line)
+    return result
+
+def test_get_line_with_out_start():
+    result = get_line_with_out_start(content)
+    assert result == ['not', 'hello from line']
--- module_re/demo_1.py
+++ module_re/demo_1.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = re.compile(r'm\w\w')
+pattern = re.compile(r'm\w\w', flags=re.IGNORECASE)
 
 demo_str = "method search from re"
 
