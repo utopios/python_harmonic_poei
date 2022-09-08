bdbfff65f8d7044ff7e1b72a6d3b78625b359017
ihab <ihab@utopios.net>
2022-09-08 15:19:17 +0200

    pandas sort

--- demo_pandas/demo-from-csv.py
+++ demo_pandas/demo-from-csv.py
@@ -3,5 +3,10 @@ import pandas as pd
 ###Read from csv
 
 df = pd.read_csv("Salaries.csv", sep=',')
-print(df[['rank','service']])
-print(df.tail(10))
+# print(df[['rank','service']])
+# print(df.tail(10))
+# print(df[15:30:3])
+# print(df.iloc[[2,3]])
+
+print(df.sort_values(by=['salary'], ascending=False))
+print(df[["salary"]].describe())
