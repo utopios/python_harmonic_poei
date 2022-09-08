694fda87e42d3b8f90f950d4fd010429cf68bb75
ihab <ihab@utopios.net>
2022-09-08 16:13:50 +0200

    correction 84

---
+++ correction_pandas/correction_slide_84.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+###Read from csv
+
+df = pd.read_csv("Salaries.csv", sep=',')
+
+#Q1
+# print(df.head(20))
+#
+# #Q2
+# print(df.tail(10))
+#
+# #Q3
+# print(df.size)
+
+#Q4
+#print(df.columns)
+
+#Q5
+# print(df.dtypes)
+
+#Q6
+#print(df[['phd','service','salary']].std())
+#print(df.select_dtypes(include='int64').std())
+
+#Q7
+print(df[['phd','service','salary']].head(50).mean())
