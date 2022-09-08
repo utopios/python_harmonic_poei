a5c996e496dea77095244ac39c3fcf21ef75472c
ihab <ihab@utopios.net>
2022-09-08 11:46:43 +0200

    ex slide 76

---
+++ correction_module_re/test_correction_ex_slide_76.py
@@ -0,0 +1,35 @@
+#1
+from re import findall, split, sub
+
+chaine = "test   : testetsestets"
+
+if(len(findall(":", chaine)) == 1):
+    print(split(":", chaine)[1])
+else:
+    print("la chaine n'a pas 1 seul ':'")
+
+#2
+chaines = [
+    "par ici",
+    "test spare",
+    "un park",
+    "sparkextragarden"
+]
+dict_replace = {
+    "par": "spar",
+    "spare": "extra",
+    "park": "garden"
+}
+
+for i in range(len(chaines)):
+    for val, repl in dict_replace.items():
+        chaines[i] = sub(r"\b" + val + r"\b", repl, chaines[i])
+print(chaines)
+
+#3
+
+
+str = "(1) test (333) ((element))"
+##Patern 'Guillaume' \(([^()]*)\)
+liste = findall(r"\((.*)\)", str)
+print(liste)
