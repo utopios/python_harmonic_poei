68327a8e00a7a3694d9116e8d3d981f68defb4df
ihab <ihab@utopios.net>
2022-09-08 09:44:08 +0200

    correction slide 68 2 et 3

--- correction_module_re/test_correction_slide_68.py
+++ correction_module_re/test_correction_slide_68.py
@@ -7,10 +7,10 @@ def repl_5_by_cinq(str):
     return re.sub('5', 'cinq', str)
 
 def repl_first_5by_cinq(str):
-    return re.sub('5', 'cinq', 1)
+    return re.sub('5', 'cinq',str, 1)
 
 def repl_note_by_x_not_sensitve(str):
-    return re.sub('note', 'X', flags=re.IGNORECASE)
+    return re.sub('note', 'X',str, flags=re.IGNORECASE)
 
 
 @pytest.fixture(scope='session')
@@ -24,3 +24,20 @@ def test_rep_5_by_cinq_should_return_str(get_data):
 def test_rep_5_by_cinq_should_return_str_without_5(get_data):
     result = repl_5_by_cinq(get_data)
     assert "5" not in result
+
+#### Test d'Armel ######
+def test_replace_first_5occurence_return_a_string(get_data):
+    result = repl_first_5by_cinq(get_data)
+    assert isinstance(result, str)
+
+def test_replace_first_5occurence_return_only_first_5(get_data):
+    result = repl_first_5by_cinq(get_data)
+    assert get_data.count("5") == result.count("5") + 1
+
+def test_replace_all_notes_by_x_return_string(get_data):
+    result = repl_note_by_x_not_sensitve(get_data)
+    assert isinstance(result, str)
+
+def test_replace_all_notes_by_x_return_no_note(get_data):
+    result = repl_note_by_x_not_sensitve(get_data)
+    assert not bool(re.search(r'note', result, flags=re.IGNORECASE))
