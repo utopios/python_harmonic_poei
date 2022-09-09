e08194f45c0013eb9ad4186db0a9492ebc82dd81
ihab <ihab@utopios.net>
2022-09-09 11:20:01 +0200

    api flask endpoints

--- cours_api_rest/app.py
+++ cours_api_rest/app.py
@@ -24,24 +24,74 @@ persons = ["toto", "tata", "titi"]
 
 ###L'ajout d'une route http à gérer par flask avec les verbs GET, POST
 ##Route pour Récupérer les personnes
-@app.route('/', methods=['GET'])
+# @app.route('/', methods=['GET'])
+# def get_persons():
+#     return persons
+#
+# ##Route pour ajouter une personne
+# @app.route('/', methods=['POST'])
+# def add_person():
+#     return 'action for add person'
+#
+# ##Route pour modifier une personne
+# @app.route('/', methods=['PUT'])
+# def update_person():
+#     return 'action for update person'
+#
+# ##Route pour delete une personne
+# @app.route('/', methods=['DELETE'])
+# def delete_person():
+#     return 'action for delete person'
+
+
+##Routes avec un niveau 1
+@app.route('/getPersons', methods=['POST'])
 def get_persons():
     return persons
 
-##Route pour ajouter une personne
-@app.route('/', methods=['POST'])
-def add_person():
-    return 'action for add person'
+@app.route('/addPerson', methods=['POST'])
+def add_persons():
+    return 'add person'
 
-##Route pour modifier une personne
-@app.route('/', methods=['PUT'])
-def update_person():
-    return 'action for update person'
+@app.route('/updatePerson', methods=['POST'])
+def update_persons():
+    return 'update person'
 
-##Route pour delete une personne
-@app.route('/', methods=['DELETE'])
+##routes avec niveau 2
+##Personnes
+@app.route('/persons', methods=['GET'])
+def get_persons():
+    return persons
+
+@app.route('/persons', methods=['POST'])
+def post_person():
+    return 'post'
+
+@app.route('/persons', methods=['PUT'])
+def put_person():
+    return 'put'
+
+@app.route('/persons', methods=['DELETE'])
 def delete_person():
-    return 'action for delete person'
+    return 'delete'
+
+##Adresses
+@app.route('/addresses', methods=['GET'])
+def get_addresses():
+    return persons
+
+@app.route('/addresses', methods=['POST'])
+def post_addresses():
+    return 'post'
+
+@app.route('/addresses', methods=['PUT'])
+def put_addresses():
+    return 'put'
+
+@app.route('/addresses', methods=['DELETE'])
+def delete_addresses():
+    return 'delete'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
