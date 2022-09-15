c71d99e152dd4c7ac0e57ef673ae66ee8c520b40
Ihab ABADI <ihababadi@MacBook-Pro-de-Ihab.local>
2022-09-15 09:46:50 +0200

    correction protection ajout de produit

--- products_orders_api_restful_with_sql_alchemy/utils/app_factory.py
+++ products_orders_api_restful_with_sql_alchemy/utils/app_factory.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_injector import FlaskInjector, request
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from repositories.genric_repository import GenericRepository
+from repositories.user_repository import UserRepository
+from resources.login_resource import LoginResource
 from resources.order_resource import OrderResource
 from resources.product_resource import ProductResource
+from resources.user_resource import UserResource
 from services.product_service import ProductService
+from services.user_service import UserService
 from utils.config import postgresql_string, postgresql_string_test
 
 
@@ -17,8 +22,13 @@ def create_app(config):
     ##Ajouter la configuration postgres à notre app
     if config["MODE"] == "TEST":
         app.config['SQLALCHEMY_DATABASE_URI'] =postgresql_string_test
+        app.config["JWT_SECRET_KEY"] = "une clé secrete pour générer le jwt en mode test"
     else:
         app.config['SQLALCHEMY_DATABASE_URI'] = postgresql_string
+        app.config["JWT_SECRET_KEY"] = "une clé secrete pour générer le jwt en mode prod"
+
+    ##Créer notre JWTManager pour notre application
+    jwt = JWTManager(app)
 
     ##Création de l'api et l'ajout des ressources, Api de flask restful est un middleware (A avoir dans en detail dans la prtie sécurité)
     api = Api(app)
@@ -29,13 +39,17 @@ def create_app(config):
         db.init_app(app)
         db.create_all()
 
-    ##ressources product
+    ##ressources product, order, user, login
     api.add_resource(ProductResource, '/products', '/products/<int:id>', endpoint='products')
     api.add_resource(OrderResource, '/orders', '/orders/<int:id>', endpoint='orders')
+    api.add_resource(UserResource, '/users',  endpoint='users')
+    api.add_resource(LoginResource, '/users/login',  endpoint='login')
 
     def flask_injector_configuration(binder):
         binder.bind(ProductService, to=ProductService, scope=request)
         binder.bind(GenericRepository, to=GenericRepository, scope=request)
+        binder.bind(UserService, to=UserService, scope=request)
+        binder.bind(UserRepository, to=UserRepository, scope=request)
 
     FlaskInjector(app=app, modules=[flask_injector_configuration])
     return app
