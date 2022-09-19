1a9c00f87e6046a64ef779cde95c7f396e53812c
Ihab ABADI <ihababadi@MacBook-Pro-de-Ihab.local>
2022-09-19 10:16:01 +0200

    cart.dll

--- ctypes_folder/demo_ctypes.py
+++ ctypes_folder/demo_ctypes.py
@@ -4,4 +4,10 @@ lib = ctypes.cdll.LoadLibrary("./c_librairies/linux/demo.so")
 ###pour windows
 # lib = ctypes.windll.LoadLibrary("./c_librairies/windows/demo.dll")
 
-print(lib.add_int(10,20))
+#print(lib.add_int(10,20)) => #utilisation de add_int de la librairies
+var1 = ctypes.c_float(10.0)
+var2 = ctypes.c_float(20.0)
+lib.add_float.argtypes = [ctypes.c_float, ctypes.c_float]
+lib.add_float.restype = ctypes.c_float
+result = lib.add_float(var1,var2)
+print(result) #=> utilisation de add_float de librairies
