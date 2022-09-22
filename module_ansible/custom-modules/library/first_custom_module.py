662ad3782e35eb0372869a48f0072edfc8261ea2
Ihab ABADI <ihababadi@MacBook-Pro-de-Ihab.local>
2022-09-22 12:26:01 +0200

    first custom module

---
+++ module_ansible/custom-modules/library/first_custom_module.py
@@ -0,0 +1,12 @@
+from ansible.module_utils.basic import *
+
+
+def main():
+    module = AnsibleModule(argument_spec={})
+
+    response = {"result": "hello world"}
+    module.exit_json(changed=False, meta=response)
+
+
+if __name__ == '__main__':
+    main()
