eb74102c60d71f612de7298705e5160f8a5a6f59
Ihab ABADI <ihababadi@MacBook-Pro-de-Ihab.local>
2022-09-22 16:10:33 +0200

    result

--- module_ansible/custom-modules/filter_plugins/custom_filter.py
+++ module_ansible/custom-modules/filter_plugins/custom_filter.py
@@ -9,7 +9,8 @@ class FilterModule(object):
         return element + ' added by custom filter'
 
     def port_filter(self, ports):
+
         result = ""
         for p in ports:
-            result += p.port + " " + p.status + " | "
+            result += str(p['port']) + " " + p['status'] + " | "
         return result
