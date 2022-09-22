cf332f48d4762046ab9e477274895c99a021bc4f
Ihab ABADI <ihababadi@MacBook-Pro-de-Ihab.local>
2022-09-22 13:55:17 +0200

    module ansible avec argument

--- module_ansible/custom-modules/library/first_custom_module.py
+++ module_ansible/custom-modules/library/first_custom_module.py
@@ -1,11 +1,28 @@
 from ansible.module_utils.basic import *
 
 
-def main():
-    module = AnsibleModule(argument_spec={})
+def logic_module(module):
+    return {
+        "response": module.params["name"] + " "+module.params["description"]
+    }
 
-    response = {"result": "hello world"}
-    module.exit_json(changed=False, meta=response)
+
+def main():
+    fields = {
+        "name": {
+            "default": "default value",
+            "type": "str"
+        },
+        "description": {
+            "required": True,
+            "type": "str"
+        }
+    }
+
+    module = AnsibleModule(argument_spec=fields)
+
+    # response = {"result": "hello world"}
+    module.exit_json(changed=False, result=logic_module(module))
 
 
 if __name__ == '__main__':
